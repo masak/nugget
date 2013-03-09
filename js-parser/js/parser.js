d40f8b2dd4f5ed46605b01178990e0ef69b2e50c
Carl Masak <cmasak@gmail.com>
2013-03-09 21:20:21 +0100

    parse prefix operators

--- js-parser/js/parser.js
+++ js-parser/js/parser.js
@@ -22,7 +22,7 @@ var parse = function (text) {
                 pos = stop;
                 return { type: 'literal', value: parseInt(number, 10) };
             }
-            if ('+*-/'.indexOf(text.charAt(pos)) > -1 ) {
+            if ('+*-/~!?'.indexOf(text.charAt(pos)) > -1 ) {
                 // we're a bit special-cased on single-char ops so far
                 var value = 'infix:<' + text.charAt(pos) + '>';
                 ++pos;
@@ -45,36 +45,79 @@ var parse = function (text) {
         var termstack = [];
         var opstack = [];
         var tightness = function(op) {
-            if (op.value === 'infix:<+>' || op.value === 'infix:<->') {
+            var any_of = function() {
+                for (var t in arguments) {
+                    if (op.value === arguments[t]) {
+                        return true;
+                    }
+                }
+                return false;
+            };
+            if (any_of('infix:<+>', 'infix:<->')) {
                 return 1;
             }
-            if (op.value === 'infix:<*>' || op.value === 'infix:</>') {
+            if (any_of('infix:<*>', 'infix:</>')) {
                 return 2;
+            }
+            if (any_of('prefix:<+>', 'prefix:<->', 'prefix:<~>', 'prefix:<!>', 'prefix:<?>')) {
+                return 3;
             }
             throw "Don't know the tightness of " + op.value;
         }
         var top_op = function() { return opstack[opstack.length - 1] }
         var reduce = function() {
             var optree = opstack.pop();
-            var rhs = termstack.pop(),
-                lhs = termstack.pop();
-            optree.children = [lhs, rhs];
+            if (optree.value.substr(0, 5) === 'infix') {
+                var rhs = termstack.pop(),
+                    lhs = termstack.pop();
+                optree.children = [lhs, rhs];
+            }
+            else { // prefix or postfix
+                var operand = termstack.pop();
+                optree.children = [operand];
+            }
             return optree;
         }
+        var still_in_expression = function() {
+            var type = tokens.next().type;
+            return type === 'operator' || type === 'literal';
+        }
 
-        termstack.push(tokens.next());
-        tokens.advance();
-        while (tokens.next().type === 'operator') {
-            var op = tokens.next();
-            if (opstack.length == 0 || tightness(top_op()) < tightness(op)) {
-                opstack.push(op);
-                tokens.advance();
+        var expect = 'term';
+        while (still_in_expression()) {
+            var type = tokens.next().type;
+            var op;
+            if (expect === 'term' && type === 'literal') {          // found term
                 termstack.push(tokens.next());
                 tokens.advance();
+                expect = 'operator';
             }
-            else {
-                termstack.push(reduce());
+            else if (expect === 'term' && type === 'operator') {    // found prefix
+                op = tokens.next();
+                op.value = 'prefix' + op.value.substr(5);
+                opstack.push(op);
+                tokens.advance();
+                expect = 'term'; // still
             }
+            else if (expect === 'operator' && type === 'literal') { // two terms in a row
+                throw "Two terms in a row: " + termstack.pop() + " " + tokens.next();
+            }
+            else if (expect === 'operator' && type === 'operator') { // found infix or postfix
+                // only handing the infix case just now
+                op = tokens.next();
+                if (opstack.length == 0 || tightness(top_op()) < tightness(op)) {
+                    opstack.push(op);
+                    tokens.advance();
+                    expect = 'term';
+                }
+                else {
+                    termstack.push(reduce());
+                    expect = 'operator'; // still
+                }
+            }
+        }
+        if (expect === 'term') {
+            throw "Expected term after " + termstack.pop();
         }
         while (opstack.length > 0) {
             termstack.push(reduce());
